2358091592c3436cb878e32cbdd6f765330911e1
fatmambo33 <51465707+fatmambot33@users.noreply.github.com>
2026-08-07 19:45:26 +0200

    Fix Compose semantics import

--- app/src/main/java/com/fatmambo33/eclipsecam/MainActivity.kt
+++ app/src/main/java/com/fatmambo33/eclipsecam/MainActivity.kt
@@ -47,11 +47,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.clearAndSetSemantics
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.stateDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
